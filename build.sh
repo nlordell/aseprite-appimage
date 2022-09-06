d6e09aa658d94e48e8762eb03f7781c02b0efc68
Kay Shinonome <kayshinonome@gmail.com>
2022-09-06 02:01:56 -0500

    Fixed for compatibility

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 echo "===> Fetching Aseprite source"
 if [[ ! -e aseprite ]]; then
-    ref=${ASEPRITE_VERSION:-main}
+    ref=${ASEPRITE_VERSION:-v1.2.40}
     git clone --recursive --depth 1 --branch $ref \
         https://github.com/aseprite/aseprite.git
 else
@@ -11,26 +11,26 @@ else
 fi
 
 mkdir -p aseprite/build
-pushd aseprite/build > /dev/null
+pushd aseprite/build >/dev/null
 
 echo "===> Retrieving pre-built Skia library"
 if [[ ! -e skia ]]; then
     rel=${SKIA_VERSION:-m102-861e4743af}
-    curl -OL https://github.com/aseprite/skia/releases/download/$rel/Skia-Linux-Release-x64.zip
-    unzip Skia-Linux-Release-x64.zip -d skia
-    rm Skia-Linux-Release-x64.zip
+    curl -OL https://github.com/aseprite/skia/releases/download/$rel/Skia-Linux-Release-x64-libstdc++.zip
+    unzip Skia-Linux-Release-x64-libstdc++.zip -d skia
+    rm Skia-Linux-Release-x64-libstdc++.zip
 else
     echo "nothing to do."
 fi
 skia=$(pwd)/skia
 
 echo "===> Building Aseprite"
-export CC=clang-10
-export CXX=clang++-10
+export CC=clang
+export CXX=clang++
 cmake \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_CXX_FLAGS:STRING=-stdlib=libc++ \
-    -DCMAKE_EXE_LINKER_FLAGS:STRING=-stdlib=libc++ \
+    -DCMAKE_CXX_FLAGS:STRING=-stdlib=libstdc++ \
+    -DCMAKE_EXE_LINKER_FLAGS:STRING=-stdlib=libstdc++ \
     -DLAF_BACKEND=skia \
     -DSKIA_DIR=$skia \
     -DSKIA_LIBRARY_DIR=$skia/out/Release-x64 \
@@ -40,8 +40,8 @@ cmake \
     -G Ninja \
     ..
 if [[ -n $ASEPRITE_VERSION ]]; then
-    echo "#define VERSION \"${ASEPRITE_VERSION#v}\"" > src/ver/generated_version.h
+    echo "#define VERSION \"${ASEPRITE_VERSION#v}\"" >src/ver/generated_version.h
 fi
 ninja aseprite
 
-popd > /dev/null
+popd >/dev/null
